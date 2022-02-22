a29a12a5289f5ecac8c2b24828e7bd39bae1241d
Kush Rawat <rawatkush0704@gmail.com>
2022-02-22 18:47:32 +0530

    modal error fixed

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,21 +5,23 @@ import Button from './Button';
 import styles from './ErrorModal.module.css';
 
 const Backdrop = (props) => {
-  <div className={styles.backdrop} onClick={props.onConfirm} />;
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
 };
 
 const ModalOverlay = (props) => {
-  <Card className={styles.modal}>
-    <header className={styles.header}>
-      <h2>{props.title}</h2>
-    </header>
-    <div className={styles.content}>
-      <p>{props.message}</p>
-    </div>
-    <footer className={styles.actions}>
-      <Button onClick={props.onConfirm}>Okay</Button>
-    </footer>
-  </Card>;
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
 };
 
 const ErrorModal = (props) => {
